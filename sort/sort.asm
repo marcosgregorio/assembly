bff093506c5b8dd7d146cda02f7b0910c277ab85
Marcos Gregorio <marquinhogreg2013@gmail.com>
2024-05-12 22:01:49 -0300

    hello world em assembly

--- sort/sort.asm
+++ sort/sort.asm
@@ -14,8 +14,8 @@ end:
 	ecall
 
 bubble_sort:
-	li t0, 0 # iteradores
-	li t1, 0
+	li t0, 0 # step
+	li t1, 0 # i
 	jal loop_step
 
 	ret
@@ -23,24 +23,34 @@ bubble_sort:
 loop_step:
 	bge t0, a1, end
 	
-	lw t2, 0(a0)
-	lw t4, 1(a0)
-	mv t3, a0 # aux pra guardar o endereco do vetor
+	lw t2, 0(a0) # valor atual
 	
+	mv t3, a0 # aux pra guardar o endereco do vetor
+	mv t5, a0 # copia para o loop interno
+	
+	addi t5, t5, 4 # salta 4 bytes ( tamanho - o passo atual )
+
 	jal ra, loop_elemento
 	
 	# somar o endereco pulando 4 bytes
 	addi t3, t3, 4
 	mv a0, t3
 
-	addi t0, t0, 1 # i + 1
+	addi t0, t0, 1 # step + 1
 	j loop_step
 
 loop_elemento:
-	blt t4, t3, swap
+	lw t4, 0(t5) # proximo valor
 	
+	blt t4, t2, swap
+	
+	addi t5, t5, 4
+	addi t1, t1, 1 # i + 1
+	
+	j loop_elemento
 	ret
 
 swap:
-	
+	sw t4, 0(a0)
+	sw t2, 1(a1)
 	ret
