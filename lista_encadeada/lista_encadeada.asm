9c96e1cd51e7646efa37acb6946af11f78f127e0
Marcos Gregorio <marquinhogreg2013@gmail.com>
2024-06-11 21:16:37 -0300

    criando insercao no final

---
+++ lista_encadeada/lista_encadeada.asm
@@ -0,0 +1,131 @@
+.data
+	mensagemMenu: .string "\n1) Insere um elemento na lista \n2) Remover elemento da lista por indice \n3) Remover elemento da lista por valor \n4) Mostrar todos os elementos da lista \n5) Mostrar estatisticas \n6) Sair do programa\n"
+	mensagemInserirElemento: .string "\nPor favor, insira um numero na lista: \n"
+	mensagemFim: .string "\nFim do programa\n"
+	mensagem: .string "\n retornado da funcao"
+	valorInserido: .string "\n Valor inserido com sucesso! Tamanho: "
+.text
+# a4 tam do minha lista
+# sp seria meu head
+main:
+	la a0, mensagemMenu
+	li a7, 4 # printa mensagem do menu
+	ecall
+	
+	li t1, 1
+	li t2, 2
+	li t3, 3
+	li t4, 4
+	li t5, 5
+	li t6, 6
+
+	li a7, 5
+	ecall
+	
+	beq a0, t1, insert_element_into_list
+	beq a0, t2, remove_element_by_index
+	beq a0, t3, remove_element_by_value
+	beq a0, t4, print_list
+	beq a0, t5, show_statistics
+	beq a0, t6, end_program
+
+insert_element_into_list:
+	la a0, mensagemInserirElemento
+	li a7, 4
+	ecall
+
+	li a7, 5
+	ecall
+
+	# copiando o valor do inserido no input
+	mv t2, a0
+
+  	# pegando o head e armazendo em a3
+	add a3, sp, zero
+	add t1, a4, zero
+	
+	jal insere_inteiro
+	
+	la a0, mensagem
+	li a7, 4
+	ecall
+	# tratar se a alocação foi feita com sucesso. Se for, deve retornar a posição da memoria.
+	# se não, retorna -1. 
+	
+	j main
+
+insere_inteiro:
+
+	# beq t1, zero, insert_into_end_of_list
+	beq t1, zero, insert_first_element
+	j insert_after
+	# carrega o proximo endereco
+	#addi a3,a3, 4
+
+	#bnez a6, insert_into_end_of_list
+	
+	ret
+
+insert_first_element:
+	add a3, sp, zero
+	li a7, 9
+
+	li a0, 8
+	ecall
+	
+	# insere o valor do input
+	sw t2, 0(a0)
+	sw zero, 4(a0)
+	
+	# sp está apontando para o primeiro endereço
+	mv sp, a0
+	
+	addi a4, a4, 1
+	
+	# printa que o valor que foi inserido e o tamanho da lista
+	la a0, valorInserido
+	li a7, 4
+	ecall
+	add a0, a4, zero
+	li a7, 1	
+	ecall
+	# corrigir esse retorno
+	j main
+insert_after:
+	lw t3, (sp)
+	lh t4, 4(sp)
+	beqz t4, insert_last
+insert_last:
+	addi t5, sp, 4
+
+	li a0, 8
+	ecall
+
+	sw t2, 0(a0)
+	sw zero, 4(a0)
+	# armazena endereco do prox
+	sw a0, 0(t5)	
+	
+# printa que o valor que foi inserido e o tamanho da lista
+
+#insert_into_end_of_list:
+#	sw a1, 0(a3)
+#	sw zero, 4(a3)
+  	#addi a4, a4, 1
+
+remove_element_by_index:
+
+remove_element_by_value:
+
+print_list:
+
+show_statistics:
+
+end_program:
+	la a0, mensagemFim
+	li a7, 4
+	ecall
+	
+	nop
+	li a7, 93
+	ecall 	
--- trabalho_org/palindromo/palindromo.asm
+++ trabalho_org/palindromo/palindromo.asm
@@ -11,24 +11,24 @@ main:
 	la a1, string
 	addi a1, a1, 5  # para ler ao contraio, eu faco o lenght da palavra - 1
 
-check_palindrome:
-	bge a0, a1, palindrome_end 
+verifica_palindrome:
+	bge a0, a1, nao_eh_palindrome 
 	# Carrega o valor de caractere atual da posição do registrador
 	lb t0, 0(a0)
 	lb t1, 0(a1)
-	beq t0, t1, continue_check # if de se os carecteres sao iguais, continua
+	beq t0, t1, continua_loop # if de se os carecteres sao iguais, continua
     	li a0, 0
-    	j end_check
+    	j end
 
-continue_check:
+continua_loop:
     	addi a0, a0, 1
     	addi a1, a1, -1
-    	j check_palindrome
+    	j verifica_palindrome
 
-palindrome_end:
+nao_eh_palindrome:
     	li a0, 1
 
-end_check:
+end:
     	li a7, 1
     	mv a0, a0
 	# printa o valor de se e 1 ou 0
